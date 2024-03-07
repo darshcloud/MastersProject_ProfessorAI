7dff44a6ca2362c5187bfee3bc53314f3ea4dde8
Darshini Venkatesha Murthy Nag <darshini.vn@gmail.com>
2024-03-07 00:46:14 -0800

    Student Home Page Integration with Backend APIs, Responsive UI fixes and added env file to gitignore

--- frontend/src/pages/studentpage/Student.js
+++ frontend/src/pages/studentpage/Student.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './student.css';
 import Navigation from "./StudentNav";
-
+import axios from 'axios';
 
 
 const Student = () => {
-  const courses = [
-    { id: 1, code: 'Course-1', name: 'Course Name 1'},
-    { id: 2, code: 'Course-2', name: 'Course Name 2'},
-    { id: 3, code: 'Course-3', name: 'Course Name 3'}
-  ];
+  const [courses, setCourses] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const studentId = 1; // Replace with actual ID coming from SSO based on login
+  const backendUrl=process.env.REACT_APP_BACKEND_URL;
 
-  const studentName = "Student Name";
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get(
+            `${backendUrl}/api/student/${studentId}/courses`, {
+              headers: {
+                'Content-Type': 'application/json',
+                //'Authorization' : `token ${localStorage.getItem('token')}`,
+                "Access-Control-Allow-Origin": "*"
+              }}
+        );
+
+        setCourses(response.data);
+        setErrorMessage('');
+      } catch (error) {
+        // catch the error message from backend
+        setErrorMessage(error.response?.data?.message || 'An unexpected error occurred while retrieving the courses.');
+      }
+    };
+
+    fetchCourses();
+  }, [studentId, backendUrl]);
+
+
+  const studentName = "Priscilla Chay Test"; //Need to retrieve student name dynamically from SSO details
 
   return (
     <div className="dashboard">
@@ -20,11 +43,12 @@ const Student = () => {
         <div className="welcome">
           <h2>Welcome {studentName}!<br/> Exciting Learning Ahead! Here is your list of enrolled courses.</h2>
         </div>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
         <div className="courses">
           {courses.map((course) => (
-            <div key={course.id} className="course">
-              <h3>{course.code}</h3>
-              <p>Course Name: {course.name}</p>
+            <div key={course.course_id} className="course">
+              <h3>{course.course_code}</h3>
+              <p>Course Name: {course.course_name}</p>
             </div>
           ))}
         </div>
